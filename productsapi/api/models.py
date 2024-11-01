e53973b1a5976360afec15c896aa8aedce68264f
gabrielrowan <gabrielrowan82@gmail.com>
2024-11-01 19:53:30 +0000

    made description optional

--- productsapi/api/models.py
+++ productsapi/api/models.py
@@ -3,7 +3,7 @@ from datetime import date
 
 class Product(models.Model):
     name = models.CharField(max_length=60)
-    description = models.TextField()
+    description = models.CharField(max_length=150, blank=True, null=True)
     # Auto_now_add automatically sets the datetime field value to now when first created
     created_date = models.DateTimeField(auto_now_add=True)
     price = models.DecimalField(max_digits=4, decimal_places=2)
@@ -11,7 +11,7 @@ class Product(models.Model):
     # blank = True allows field to be left blank when object is created
     # null = True means empty values will be stored as null in the db
     sale_price = models.DecimalField(max_digits=4, decimal_places=2, blank=True, null=True)
-    sale_expiry_date = models.DateField(blank=True, null=True, default=date.today)
+    sale_expiry_date = models.DateField(blank=True, null=True)
     
     # Defines the string representation of an instance of a model
     # In this instance, the string representation is set to the name field of the model
