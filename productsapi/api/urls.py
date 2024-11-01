abe4112e4b34e52ade8c8cf2e9e556312195cd05
gabrielrowan <gabrielrowan82@gmail.com>
2024-11-01 13:54:57 +0000

    corrected syntax error in urls.py

--- productsapi/api/urls.py
+++ productsapi/api/urls.py
@@ -4,19 +4,15 @@ from . import views
 # Contains a mapping between URL patterns and Python callback functions in the form of views
 
 urlpatterns = [
-    ''' 
-    Url for:
-        - Viewing all products
-        - Creating a product 
-        - Deleting all products
-    '''
+    # Url for:
+    #     - Viewing all products
+    #     - Creating a product 
+    #     - Deleting all products
     path("products/", views.ProductListCreate.as_view(), name="viewall-create-deleteall"),
     
-    ''' 
-    Url for:
-        - Fetting data for a given product based on the primary key
-        - Updating a product 
-        - Deleting a product
-    '''
+    # Url for:
+    #     - Getting data for a given product based on the primary key
+    #     - Updating a product 
+    #     - Deleting a product
     path("products/<int:pk>/", views.ProductPostRetrieveUpdateDestroyView.as_view(), name="get-update-delete")
 ]
